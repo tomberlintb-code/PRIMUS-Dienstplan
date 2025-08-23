f6a003ba7b579e5fd3027de49a55d8dd1c3b073f
Tom Berlin <tomberlintb@googlemail.com>
2025-08-23 19:49:01 +0200

    Fix: richtiger Importpfad für useUserRole

--- src/app/planung/[year]/[month]/page.tsx
+++ src/app/planung/[year]/[month]/page.tsx
@@ -17,7 +17,7 @@ import {
   where,
 } from "firebase/firestore";
 import { db } from "../../../../lib/firebase";
-import useUserRole from "../../../../lib/useUserRole";
+import useUserRole from "../../../lib/useUserRole";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 import styles from "./planung.module.css";
